543de5f401024c56d5462eccc0b22780ea911a29
naimulnihal2696@gmail.com <naimulnihal2696@gmail.com>
2021-08-01 17:53:36 +0600

    4th upload

--- assets/js/app.js
+++ assets/js/app.js
@@ -17,6 +17,28 @@ $(function(){
         slidesToShow:4,
         prevArrow:'<i class="fas fa-chevron-left latest_arrow latest_left"></i>',
         nextArrow:'<i class="fas fa-chevron-right latest_arrow latest_right"></i>',
+
+        responsive:[
+           {   breakpoint:992,
+               settings:{
+                 slidesToShow:3,
+                 slidesToScroll:1,
+               }
+           },
+           {   breakpoint:576,
+            settings:{
+              slidesToShow:2,
+              slidesToScroll:1,
+            }
+        },
+           {   breakpoint:400,
+            settings:{
+              slidesToShow:1,
+              slidesToScroll:1,
+            }
+        }
+          
+        ]
     })
 
 
@@ -27,15 +49,24 @@ $(function(){
 
     //countdown js
 
-   $('.cont_down').countdown('2021/10/10', function(event) {
+   $('.cont_down').countdown('10/10/2022', function(event) {
     var $this = $(this).html(event.strftime(''
-      + '<div class="col-sm-3"><div class="cont_down_item"><span>%d</span><span>days</span></div></div> '
-      + '<div class="col-sm-3"><div class="cont_down_item"><span>%H</span><span>hour</span></div></div> '
-      + '<div class="col-sm-3"><div class="cont_down_item"><span>%M</span><span>min</span></div></div> '
-      + '<div class="col-sm-3"><div class="cont_down_item"><span>%S</span><span>secs</span></div></div> '
+      + '<div class="col-3"><div class="cont_down_item"><span>%d</span><span>days</span></div></div> '
+      + '<div class="col-3"><div class="cont_down_item"><span>%H</span><span>hour</span></div></div> '
+      + '<div class="col-3"><div class="cont_down_item"><span>%M</span><span>min</span></div></div> '
+      + '<div class="col-3"><div class="cont_down_item"><span>%S</span><span>secs</span></div></div> '
 
      ));
   });
-        
-    
+      
+})
+
+$(function(){
+  $(window).on('scroll',function(){
+    if($(window).scrollTop()>0){
+      $('#mynav').addClass('stickynav')
+    }else{
+      $('#mynav').removeClass('stickynav')
+    }
+  })
 })
